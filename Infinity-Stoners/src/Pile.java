c7ba162b9dfe584fef824224d30fff242ee5a7ab
Zhiheng Chang <Zhihengchang602@gmail.com>
2018-11-05 12:53:12 -0500

    Project Skeleton Code
    class added:
    Card (full implementation)
    Pile (full implementation)
    Deck
    Player
    Game
    Runner

---
+++ Infinity-Stoners/src/Pile.java
@@ -0,0 +1,77 @@
+/**
+ * 
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author 
+ *
+ */
+public class Pile {
+	private static List<Card> cards;
+	/**
+	 * 
+	 */
+	public Pile() {
+		cards = new ArrayList<Card>(52);
+		for(Rank aRank : Rank.values()) {
+			for(int i=0; i<4; i++) {
+				if(aRank.getDisplayName().equals("Joker")) {
+					break;
+				}
+				cards.add(new Card(aRank.getGraphic(), aRank.getPriority()));
+			}
+		}
+		shuffle();
+		
+	}
+	private void shuffle() {
+		ArrayList<Card> shuffledPile = new ArrayList<Card>();
+		
+		while (cards.size() > 0) {
+			int index = (int) (Math.random() * cards.size());
+	        shuffledPile.add(cards.remove(index));   
+			}
+				
+		cards = shuffledPile;
+	}
+	
+	public void add(Card o) {
+		cards.add(o);
+	}
+	
+	public Card remove() {
+		return cards.remove(0);
+	}
+	
+	
+	public int getSize() {
+		return cards.size();
+	}
+	
+	public String toString() {
+		String str = "";
+		for(Card o : cards) {
+			str += o.getGraphic() + " ";
+		}
+		return str;
+	}
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Pile p = new Pile();
+		p.shuffle();
+		p.shuffle();
+		System.out.println(p.toString());
+		System.out.println(p.getSize());
+		
+
+	}
+
+}
---
+++ Infinity-Stoners/src/Rank.java
@@ -0,0 +1,51 @@
+
+public enum Rank {
+	//Element   Display Name    Graphic     Priority 
+	ACE     (   "Ace",          "A",           1),
+	TWO     (   "Duece",        "2",           2),
+	THREE   (   "Three",        "3",           3),
+	FOUR    (   "Four",         "4",           4),
+	FIVE    (   "Five",         "5",           5),
+	SIX     (   "Six",          "6",           6),
+	SEVEN   (   "Seven",        "7",           7),
+	EIGHT   (   "Eight",        "8",           8),
+	NINE    (   "Nine",         "9",           9),
+	TEN     (   "Ten",          "10",         10),
+	JACK    (   "Jack",         "J",          11),
+	QUEEN   (   "Queen",        "Q",          12),
+	KING    (   "King",         "K",          13),
+	JOKER   (   "Joker",        "R",          99);
+
+	private final String displayName;
+	private final String graphic;
+	private final int priority;
+
+	private Rank(String rankDisplayName, String rankGraphic,int rankPriority) {
+		displayName =   rankDisplayName;
+		graphic =       rankGraphic;
+		priority =      rankPriority;
+	} // end constructor
+
+	public String getDisplayName() {
+		return displayName;
+	} // end getDisplayName
+
+	public String getGraphic() {
+		return graphic;
+	} // end getGraphic
+
+	public int getPriority() {
+		return priority;
+	} // end getPriority
+
+	@Override
+	public String toString() {
+		return graphic ;
+	}
+
+
+    public static void main(String[] args) {
+        
+    }
+
+} // end Rank
