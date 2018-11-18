b98ccff0e8f523c50e10960c7cf8419a9c3e7ffb
Zhiheng Chang <Zhihengchang602@gmail.com>
2018-11-18 15:07:27 -0500

    full player class code

--- Infinity-Stoners/src/Player.java
+++ Infinity-Stoners/src/Player.java
@@ -1,5 +1,3 @@
-
-
 public class Player {
 	private Deck deck;
 	private Deck removedCards;
@@ -13,10 +11,133 @@ public class Player {
 		win = false;
 		atWar = false;
 	}
-
-	/**
-	 * @param args
+	
+	/*
+	 * add a deck to the player
+	 * @param a deck object
 	 */
+	public void addDeck(Deck deck) {
+		this.deck = deck;
+	}
+	
+	/*
+	 * check is there any card left in player's deck
+	 * @return true or false
+	 */
+	public boolean noCardsLeft() {
+		return deck.isEmpty();
+	}
+	
+	/*
+	 * get player's name
+	 * @return player's name
+	 */
+	public String getName() {
+		return name;
+	}
+	
+	/*
+	 * set player's name
+	 * @param player's name
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*
+	 * reveal a card for comparison
+	 * remove the top card of the player's deck
+	 * add this card into removed cards deck
+	 * @return top card (removed card)
+	 */
+	public Card revealACard() {
+		Card removedCard = deck.removeTop();
+		removedCards.addCardToDeck(removedCard);
+		return removedCard;
+	}
+	
+	/*
+	 * get all the removed cards
+	 * @return removed cards deck
+	 */
+	public Deck getRemovedCards() {
+		return removedCards;
+	}
+	
+	/*
+	 * get the number of cards in player's deck
+	 * @return the number of cards in player's deck
+	 */
+	public int getNumberOfCardsInDeck() {
+		return deck.getNumberOfCards();
+	}
+	
+	/*
+	 * get number of cards in the removed cards deck
+	 * @return number of cards in removed cards deck
+	 */
+	public int getNumberOfRemovedCards() {
+		return removedCards.getNumberOfCards();
+	}
+	
+	/*
+	 * check is player at war
+	 * @return true or false
+	 */
+	public boolean isAtWar() {
+		return atWar;
+	}
+	
+	/*
+	 * set the status of war for player
+	 * @param true or false
+	 */
+	public void setWar(boolean atWar) {
+		this.atWar = atWar;
+	}
+	
+	/*
+	 * if player wins the war, player will get another player's removed cards deck
+	 * and add all the cards in removed cards deck to this player's removed cards deck
+	 * @param opponent's removed cards deck
+	 */
+	public void gainCards(Deck removedCards) {
+		while(!removedCards.isEmpty()) {
+			this.removedCards.addCardToDeck(removedCards.removeTop());
+		}
+	}
+	
+	/*
+	 * add all the cards in the removed cards to player's deck
+	 */
+	public void addAllRemovedCardsToDeck() {
+		while(!this.removedCards.isEmpty()) {
+			deck.addCardToDeck(this.removedCards.removeTop());
+		}
+	}
+	
+	/*
+	 * check is the player the winner of the game
+	 * @return true or false
+	 */
+	public boolean isWinner() {
+		return win;
+	}
+	
+	/*
+	 * set this player as the winner of the game
+	 */
+	public void setWinner() {
+		win = true;
+	}
+	
+	public String toString() {
+		String str = "Player name: " + name + "\n";
+		str += deck.toString() + "\n";
+		str += "Number of cards in deck: " + deck.getNumberOfCards();
+		return str;
+	}
+
 	public static void main(String[] args) {
 
 	}
